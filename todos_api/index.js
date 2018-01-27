150f54bcb9a21cb11600456f9616688be0bfb934
Mauro <bono.mauro@gmail.com>
2018-01-27 17:31:47 +0100

    add body-parser

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {res.send(`HELLO FROM ROOT ROUTE!`)});
 
 app.use('/api/todos', todoRoutes);
 /***********************************************************************/
-            /*********************SERVER*****************************/
+        /*********************SERVER*****************************/
 /***********************************************************************/
 app.listen(PORT, () => {console.log(`Server is running on PORT: ${PORT}`)});
 
