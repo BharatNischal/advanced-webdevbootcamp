310af3b272e56061765f0489b25e2539a3dac92a
Mauro <bono.mauro@gmail.com>
2018-01-27 15:53:23 +0100

    update index.js with res.json method

--- todos_api/index.js
+++ todos_api/index.js
@@ -3,6 +3,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => {res.send(`Hi there from Express!`)});
+app.get('/', (req, res) => {res.json({message: `Hi there from Express!`})});
 
 app.listen(PORT, () => {console.log(`Server is running on PORT: ${PORT}`)});
