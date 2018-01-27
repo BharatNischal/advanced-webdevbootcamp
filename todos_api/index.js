79efb0f3073840c2fe67c47cfa22bbea8b33d26f
Mauro <bono.mauro@gmail.com>
2018-01-27 17:09:04 +0100

    update app structure, add mongoose, Schema, express.Router and routes folder with get route

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const todoRoutes = require('./routes/todos');
 
+app.get('/', (req, res) => {res.send(`HELLO FROM ROOT ROUTE!`)});
 
-app.get('/', (req, res) => {res.json({message: `Hi there from Express!`})});
-
+app.use('/api/todos', todoRoutes);
+/***********************************************************************/
+            /*********************SERVER*****************************/
+/***********************************************************************/
 app.listen(PORT, () => {console.log(`Server is running on PORT: ${PORT}`)});
+
+//sudo service mongodb start: this is the command line to start mongodb on ubuntu
+//sudo service mongodb stop: this is the command line to stop mongodb on ubuntu
