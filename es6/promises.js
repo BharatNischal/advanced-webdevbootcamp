5afc31d5c11eae921a6327f33faf59c5417121eb
Mauro Bono <bono.mauro@gmail.com>
2018-02-08 11:48:17 +0100

    update promises.js with some exercises

--- es6/promises.js
+++ es6/promises.js
@@ -38,3 +38,38 @@ fetch('https://omdbapi.com?t=titanic&apikey=thewdb')
 .then(movie => console.log(movie.Year))
 .catch(err => console.log(err));
 
+
+//PROMISE ALL
+function getMovie(title) {
+    //we use JQuery to fetch data from OMDB ...
+    return $.getJSON(`https://omdbapi.com?t=${title}&apikey=thewdb`);
+}
+var braveheartPromise = getMovie('braveheart');
+var titanicPromise = getMovie('titanic');
+var shrekPromise = getMovie('shrek');
+
+Promise.all([braveheartPromise, titanicPromise, shrekPromise])
+.then(movies => movies.forEach(value => console.log(value.Year)));
+
+//EXERCISES
+function getMostFollowers(...usernames) {
+    let baseUrl = "https://api.github.com/users/";
+    let urls = usernames.map(username => $.getJSON(baseUrl + username));
+    return Promise.all(urls)
+    .then(data => {
+        let max = data.sort((a,b) => a.followers < b.followers)[0];
+        return `${max.name} has the most followers with ${max.followers}`;
+    });
+}
+
+getMostFollowers('elie', 'tigarcia', 'colt').then(function (data) {
+    console.log(data)
+});
+
+function starWarsString(num) {
+    return $.getJSON(`https://swapi.co/api/people/${num}/`);
+}
+
+starWarsString(2)
+.then(data => console.log(data));
+
