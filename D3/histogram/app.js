f2a199117cf1a562cbe63563b62a8cf74dd608c3
Mauro <bono.mauro@gmail.com>
2018-02-19 21:53:59 +0100

    add histogram project

--- D3/histogram/app.js
+++ D3/histogram/app.js
@@ -1,27 +1,26 @@
 const width = 600;
 const height = 600;
 const barPadding = 1;
-
+const padding = 20;
 const minYear = d3.min(birthData, d => d.year);
-const yearData = birthData.filter(d => d.year === minYear);
+const maxYear = d3.max(birthData, d => d.year);
+let yearData = birthData.filter(d => d.year === minYear);
 
-const xScale = d3.scaleLinear()
+let xScale = d3.scaleLinear()
                  .domain([0, d3.max(yearData, d => d.births)])
-                 .rangeRound([0, width]);
+                 .rangeRound([padding, width - padding]);
 
-const histogram = d3.histogram()
+let histogram = d3.histogram()
                     .domain(xScale.domain())
                     .thresholds(xScale.ticks())
                     .value(d => d.births);
-const bins = histogram(yearData);
+let bins = histogram(yearData);
 
-const barWidth = width / bins.length - barPadding;
-
-const yScale = d3.scaleLinear()
+let yScale = d3.scaleLinear()
                  .domain([0, d3.max(bins, d => d.length)])
                  .range([height, 0]);
 
-const bars = d3.select('svg')
+let bars = d3.select('svg')
                     .attr('width', width)
                     .attr('height', height)
                 .selectAll('.bar')
@@ -46,3 +45,49 @@ bars
     .attr("x", - height + 10)
     .style("alignment-baseline", "middle");
 
+d3.select('input')
+    .property('min', minYear)
+    .property('max', maxYear)
+    .property('value', minYear)
+    .on('input', function() {
+        let year = +d3.event.target.value;
+        //begin of the UPDATE PATTERN
+        yearData = birthData.filter(d => d.year === year);
+        xScale.domain([0, d3.max([yearData, d => d.births])]);
+        histogram.domain(xScale.domain())
+                 .thresholds(xScale.ticks());
+        bins = histogram(yearData);
+        yScale.domain([0, d3.max(bins, d => d.length)]);
+
+        bars = d3.select('svg')
+                    .selectAll('bar')
+                    .data(bins);
+        bars
+            .exit()
+            .remove();
+        
+        const g = bars.enter().append('g')
+                                .classed('bar', true);
+        g.append('rect');
+        g.append('text');
+
+        g.merge(bars)
+            .select('rect')
+            .attr('x', (d,i) => {
+                console.log(xScale(d.x0));
+                return xScale(d.x0)
+            })
+            .attr('y', d => yScale(d.length))
+            .attr('height', d => height - yScale(d.length))
+            .attr('width', d => xScale(d.x1) - xScale(d.x0) - barPadding)
+            .attr('fill', 'green');
+
+        g.merge(bars)
+            .select('text')
+            .text(d => d.x0 + " - " + d.x1 + " (bar height: " + d.length + ")")
+            .attr("transform", "rotate(-90)")
+            .attr("y", d => (xScale(d.x1) + xScale(d.x0)) / 2)
+            .attr("x", - height + 10)
+            .style("alignment-baseline", "middle");
+    
+});
